6e35730861d181943b3111f401a02d03f6692fdc
Ali Bayat <ali.bayat77@yahoo.com>
2023-03-05 18:22:43 +0330

    FormModule added

--- src/app/contacts-form/contacts-form.component.ts
+++ src/app/contacts-form/contacts-form.component.ts
@@ -3,8 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'contacts-form',
   templateUrl: './contacts-form.component.html',
-  styleUrls: ['./contacts-form.component.scss']
+  styleUrls: ['./contacts-form.component.scss'],
 })
 export class ContactsFormComponent {
-
+  log(x: any) {
+    console.log(x);
+  }
 }
